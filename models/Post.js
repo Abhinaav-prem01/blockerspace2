c9bc849f035130104a3254ef095e353e9e17b717
Abhinaav-prem01 <abhinaavp@gmail.com>
2026-02-19 15:47:24 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  userId: String,
-  username: String,
-  content: String,
-  likedBy: [String],
-  createdAt: { type: Date, default: Date.now }
+  content: { type: String, required: true },
+  likedBy: { type: [String], default: [] },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
