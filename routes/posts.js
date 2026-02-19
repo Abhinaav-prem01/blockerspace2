801f0f22a5f7420d83f36f80bb5711faed5e542f
Abhinaav-prem01 <abhinaavp@gmail.com>
2026-02-19 15:46:34 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,33 +1,25 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
 
-// Create post
-router.post("/", async (req, res) => {
-  const post = new Post(req.body);
-  await post.save();
-  res.json(post);
-});
-
-// Get all posts
+// get all posts
 router.get("/", async (req, res) => {
-  const posts = await Post.find().sort({ createdAt: -1 });
-  res.json(posts);
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// Like post
-router.put("/:id/like", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-
-  const { email } = req.body;
-
-  if (post.likedBy.includes(email)) {
-    post.likedBy = post.likedBy.filter(e => e !== email);
-  } else {
-    post.likedBy.push(email);
+// create post
+router.post("/", async (req, res) => {
+  try {
+    const newPost = new Post(req.body);
+    const saved = await newPost.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  await post.save();
-  res.json(post);
 });
 
 module.exports = router;
